a7050e058055090128ce30e54f907c79598237a0
Karancse <83109484+Karancse@users.noreply.github.com>
2021-07-15 19:51:49 +0530

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const app = express()
+var fs = require('fs');
+//var http = require('http');
+//var https = require('https');
+//var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+//var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+
+/*
+var credentials = {
+    key: fs.readFileSync('ssl/keys/server.key'),
+    cert: fs.readFileSync('ssl/keys/server.crt')
+};
+*/
+
+const mysql = require('mysql')
+
+const db = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'reelearn'
+})
+
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql connected...\n');
+});
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.post("/signUp", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let SQL = "INSERT INTO userCredentials (username, password) VALUE ("+username+","+password+")"; 
+    db.query(SQL, (err, result) => {
+        if(err) throw err;
+        res.send('Valid');
+    });
+})
+
+app.post("/logIn", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let SQL = "SELECT * FROM userCredentials WHERE username = "+username+" AND password = "+password;
+    db.query(SQL, (err, result) => {
+        if(err) throw err;
+        if(result.length==1)
+            res.send({
+                status: 'Valid'
+            });
+        else
+            res.send({
+                status: 'Invalid'
+            })
+    });
+})
+
+//var httpsServer = https.createServer(credentials, app);
+
+//httpsServer.listen(3001);
+
+app.listen(3001,() => {
+    console.log('\nListening to localhost:3001');
+});
