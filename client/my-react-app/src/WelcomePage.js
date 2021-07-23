338431ad6b1287e32c9d3168d3fcbe9664bbfb27
Karancse <83109484+Karancse@users.noreply.github.com>
2021-07-23 16:19:53 +0530

    Update WelcomePage.js

--- client/my-react-app/src/WelcomePage.js
+++ client/my-react-app/src/WelcomePage.js
@@ -8,9 +8,11 @@ class WelcomePanel extends React.Component {
     render() {
         return (
             <div className='welcomePanel'>
-                <button className='logIn' onClick={() => this.props.onClick('logIn')}>LogIn</button>
-                <button className='signUp' onClick={() => this.props.onClick('signUp')}>SignUp</button>
-                <button className='homePage' onClick={() => this.props.onClick('homePage')}>Home</button>
+                <button className='logInNav' onClick={() => this.props.onClick('logIn')}>LogIn</button>
+                <button className='signUpNav' onClick={() => this.props.onClick('signUp')}>SignUp</button>
+                <button className='homePageNav' onClick={() => this.props.onClick('homePage')}>Home</button>
+                <button className='postDetailsNav' onClick={() => this.props.onClick('postDetails')}>Post</button>
+                <button className='uploadNowNav' onClick={() => this.props.onClick('uploadNow')}>Upload Now</button>
             </div>
         )
     }
