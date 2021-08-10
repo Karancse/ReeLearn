9c7338cc1bcaab53c09c74727ae8749720226a15
Karancse <83109484+Karancse@users.noreply.github.com>
2021-08-10 17:41:46 +0530

    Update index.js

--- serverMongoDB/index.js
+++ serverMongoDB/index.js
@@ -1,187 +1,167 @@
-//const mongoose = require ('mongoose');
-
-const mongoose = require('mongoose');
-
-const { Router } = require ('express');
-const router = Router();
-
-const express = require('express')
-const app = express()
+import React from 'react';
+import './logInPageStyle.css';
+import Axios from 'axios';
+import { reactLocalStorage } from 'reactjs-localstorage';
 
 
-const session = require('express-session')
+function EnterUsername (props) {
+  return (
+    <div className="enterUsername">
+      <span>Enter Username:</span>
+      <input className='usernameInput' onChange = { event => props.onChange(event.target.value) } type='text' placeholder='Your Username'></input>
+    </div>
+  )
+}
 
-app.use(express.static("public"));
-app.use(express.json());
+function EnterEmail (props) {
+  return (
+    <div className="enterEmail">
+      <span>Enter Email:</span>
+      <input className='emailInput' onChange = { event => props.onChange(event.target.value) } type='text' placeholder='Your Email'></input>
+    </div>
+  )
+}
 
-var fs = require('fs');
-
-const cors = require('cors');
-app.use(cors())
-
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
-
-app.use(session({
-	secret: 'secret',
-	reSave: false,
-	saveUninitialized: false
-}))
-
-app.use(passport.initialize())
-app.use(passport.session())
-
-//import { Schema } from 'mongoose';
-
-const Schema = mongoose.Schema;
-
-const model = mongoose.model;
-
-const CredentialSchema = Schema({
-	username: {
-		type: String,
-		required: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	email: {
-		type: String,
-		required: true,
-	}
-});
-
-const password = 'asdfpoiu1234';
-var uri = "mongodb+srv://Karan:"+password+"@reelearn.lrvqf.mongodb.net/userDetails?retryWrites=true&w=majority"
-//var uri = "mongodb://Karan:"+password+"@clustermasjeed1-shard-00-00-ekpfe.mongodb.net:27017,clustermasjeed1-shard-00-01-ekpfe.mongodb.net:27017,clustermasjeed1-shard-00-02-ekpfe.mongodb.net:27017/test?ssl=true&replicaSet=ClusterMasjeed1-shard-0&authSource=admin&retryWrites=true"
+function EnterPassword (props) {
+  return (
+    <div className="enterPassword">
+      <span>Enter Password:</span>
+      <input className='passwordInput' onChange = { event => props.onChange(event.target.value) } type='text' placeholder='Your Password'></input>
+    </div>
+  )
+}
 
 
-const connectDB = async () => {
-	try {
-        console.log(9);
-		mongoose.connect(uri, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useFindAndModify: false,
-			useCreateIndex: true,
-		}).then(res => {
-            console.log(16);  
-			/*console.log(res);*/
+function SubmitButton (props) {
+  return (
+    <div className='submitDetails'>
+      <button className='submitButton' onClick = { () => props.onClick() } type='submit'>Submit</button>
+    </div> 
+  )
+}
+
+class LoginPanel extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username : '',
+      email: '',
+      password : '',
+      status: ''
+    };
+  }
+
+  UpdateUsername(username) {
+    this.setState({
+      username : username
+    });
+  }
+
+  UpdateEmail(email){
+    this.setState({
+      email: email
+    })
+  }
+
+  UpdatePassword(password) {
+    this.setState({
+      password : password
+    });
+  }
+
+  Submit() {
+
+    if(this.state.email==null || this.state.email==='')
+    {
+      this.setState({
+        status:'Enter email'
+      });
+      return;
+    }
+
+    if(this.state.password==null || this.state.password==='')
+    {
+      this.setState({
+        status: 'Enter password'
+      });
+      return;
+    }
+    /*
+    const data = { 
+      username: this.state.username,
+      password: this.state.password
+    };
+
+    const options = {
+      method: 'POST',
+      url: 'http://localhost:3001/logIn',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+      //body1: data
+    };
+
+    //fetch('/logIn',options);
+    Axios(options).then(res => {
+      this.setState({
+        status: res.data.status
+      })
+    });
+    */
+    
+    Axios.post("http://localhost:3001/logIn", {
+      email: this.state.email,
+      password: this.state.password
+    }, { 
+      headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    } 
+  }).then(res => {
+        console.log(res);
+        this.setState({
+          status: res.data.status
+        });
+        reactLocalStorage.set( 'username' , res.data.username );
+        this.setState({
+          status: reactLocalStorage.get('username')
         })
-	} catch (err) {
-		console.error(err.message);
-		process.exit(1);
-	}
-};
+  });
+  
+/*
+  Axios({
+    method: 'post',
+    url: 'https://localhost:3001/logIn',
+    data: {
+      firstName: this.state.username,
+      lastName: this.state.password
+    }
+  });
+*/
 
-connectDB();
+}
 
-var Credential = model('credential', CredentialSchema);
-
-	
-	app.post("/signUp", async (req, res) => {
-		const { username, password, email } = req.body;
-
-		console.log("SignUp Request: "+username+" "+password+" "+email)
-	
-		credential = await Credential.findOne({ email })
-		
-		if (credential) {
-				console.log("An User with the EmailID Already Exists")
-				res.send({
-					status: 'An User with the EmailID Already Exists'
-				})
-				return
-		}
-
-		var credential = new Credential({
-				username,
-				password,
-				email
-		});
-	
-		await credential.save()
-		
-		console.log('saved');
-		return(
-			res.send({
-				status: 'Signed Up'
-			})
-		)
-	})
-	
-	app.post("/logIn1", async (req, res) => {
-		const { username, password } = req.body;
-
-		console.log("LogIn Request: "+username+" "+password)
-
-		credential = await Credential.findOne({ username , password })
-
-		console.log(credential)
-
-		if (!credential) {
-				console.log("Invalid")
-				res.send({
-					status: 'Invalid'
-				})
-		}
-
-		if(credential)
-		{
-			console.log("Valid")
-			res.send({
-				status: 'Valid'
-			})
-		}
-	})
-
-	function initializePassport(passport) {
-		const authenticateUser = async (email , password, done) => {
-			credential = await Credential.findOne({ email })
-	
-			if(!credential) {
-				done(null, false, { message: "User doesn't exist" } )
-			}
-	
-			if ( credential.password!=password ){
-				done(null, false, { message: "Incorrect Password" } )
-			}
-	
-			done(null, credential)
-	
-		}
-	
-		passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
-		passport.serializeUser((user, done) => {  done(null, user.id) })
-		passport.deserializeUser((id, done) => {  done(null, Credential.findById(id)) })
-	}
-	
-	
-	
-	initializePassport(passport)
-	
-
-	app.post("/logIn", passport.authenticate('local', {
-		successRedirect: '/',
-		failureRedirect: '/inValid',
-		})
-	)
-	
-	app.get('/', (req, res) => {
-		res.send({
-			status: 'Valid'
-		})
-	})
-
-	app.get('/inValid',(req, res) => {
-		res.send({
-			status: 'Invalid'
-		})
-	})
+  render() {
+    return (
+      <div className="logInPanel">
+        <h3>LogIn Page</h3>
+        <EnterEmail email={ this.state.email } onChange={email => this.UpdateEmail(email) }/>
+        <EnterPassword password={ this.state.password } onChange={password => this.UpdatePassword(password) }/>
+        <SubmitButton onClick={() => this.Submit() }/>
+        <p>{ this.state.status }</p>
+      </div>
+    );
+  }
+}
 
 
-	app.listen(3001,() => {
-		console.log('\nListening to localhost:3001');
-	});
+function LoginPage() {
+  return (
+    <div className="logInPage">
+      <LoginPanel />
+    </div>
+  );
+}
 
+export default LoginPage;
